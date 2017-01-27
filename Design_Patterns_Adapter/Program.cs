a1c51e6342ed7d989ec1532a37e7bcf98013a775
TaharLOUKIL <loukil.tahar@gmail.com>
2017-01-27 22:39:46 +0100

    First Commit

--- Design_Patterns_Adapter/Program.cs
+++ Design_Patterns_Adapter/Program.cs
@@ -1,15 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Design_Patterns_Adapter
 {
+    /// <summary>
+    /// MainApp startup class for Real-World 
+    /// Adapter Design Pattern.
+    /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
         {
+            // Non-adapted chemical compound
+            Compound unknown = new Compound("Unknown");
+            unknown.Display();
+
+            // Adapted chemical compounds
+            Compound water = new RichCompound("Water");
+            water.Display();
+
+            Compound benzene = new RichCompound("Benzene");
+            benzene.Display();
+
+            Compound ethanol = new RichCompound("Ethanol");
+            ethanol.Display();
+
+            // Wait for user
+            Console.ReadKey();
         }
     }
 }
